11d352cca749d2b1f9e480370fa0b667f3f3d516
Jorge <jorgeteixeiramelo@gmail.com>
2021-05-11 22:40:25 -0300

    Resolução do Ex01 da lista da aula 6

---
+++ Aula_6/Ex01.py
@@ -0,0 +1,9 @@
+#Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+def soma(a, b, c):
+    soma = a + b + c
+    return soma
+a = int(input('Digite o 1° número:'))
+b = int(input('Digite o 2° número:'))
+c = int(input('Digite o 3° número:'))
+print('A soma entre esse números é:',soma(a, b, c))
