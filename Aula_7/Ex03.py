70e0263fd2f457a73206e3b34d66e55d2719d5d6
Jorge <jorgeteixeiramelo@gmail.com>
2021-05-13 11:59:48 -0300

    Resolução dos exercícios da lista da aula 7

---
+++ Aula_7/Ex03.py
@@ -0,0 +1,5 @@
+#Crie uma função que receba uma string como argumento e retorne a mesma string em letras maiúsculas. Faça uma chamada à função, passando como parâmetro uma string
+def min(string):
+    return string.lower()
+string = str(input('Digite uma frase:'))
+print(min(string))
