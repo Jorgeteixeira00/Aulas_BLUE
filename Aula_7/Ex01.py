4a574474c30781231b67ebdbf395e10b2a8087d5
Jorge <jorgeteixeiramelo@gmail.com>
2021-05-13 11:47:49 -0300

    Resolução dos exercicios da lista da aula 7

---
+++ Aula_7/Ex01.py
@@ -0,0 +1,11 @@
+#Escreva uma função que recebe dois parâmetros (números) e imprime o menor dos dois. Se eles forem iguais, imprima que são valores idênticos.
+def menor(v1,v2):
+    if n1 < n2:
+        print('{} é o menor!'.format(n1))
+    elif n2 < n2:
+        print('{} é o menor!'.format(n2))
+    else:
+        print('São iguais!')
+n1 = int(input('Digite o primeiro número:'))
+n2 = int(input('Digite o segundo número:'))
+menor(n1,n2)
