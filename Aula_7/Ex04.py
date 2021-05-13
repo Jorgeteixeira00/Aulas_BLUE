caa224924ff564d09f30cf1d784bbcc2fbb8ab07
Jorge <jorgeteixeiramelo@gmail.com>
2021-05-13 12:45:17 -0300

    Resolução dos exercícios da lista da aula 7

---
+++ Aula_7/Ex04.py
@@ -0,0 +1,14 @@
+#Crie um programa que tenha uma função chamada voto () que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO nas eleições. Para resolver esse exercício, pesquise sobre a função date da biblioteca Datetime.
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade <16:
+        return f'Com {idade} anos VOTO:NEGADO'
+    elif idade >=16 and idade <18:
+        return f'Com {idade} anos VOTO:OPCIONAL'
+    else:
+        return f'Com {idade} anos VOTO:OBRIGATORIO'
+
+nasc = int(input('Em que ano você nasceu:'))
+print(voto(nasc))
