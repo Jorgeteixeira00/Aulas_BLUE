2bc519aef85ad93efc231dadd1f2ac5bfa80b20c
Jorge <jorgeteixeiramelo@gmail.com>
2021-05-13 11:54:45 -0300

    Resolução dos exercicios da lista da aula 7

---
+++ Aula_7/Ex02.py
@@ -0,0 +1,10 @@
+#Escreva uma função que recebe dois números (a e b) como parâmetro e retorna True caso a soma dos dois seja maior que um terceiro parâmetro, chamado limite.
+def maior_soma(a, b, l):
+    if n1 + n2 > limite:
+        print('A soma entre {} e {} é maior que o limite!'.format(n1,n2))
+    else:
+        print('A soma entre {} e {} não é maior que o limite!'.format(n1,n2))
+n1 = int(input('Digite o primeiro número:'))
+n2 = int(input('Digite o segundo número:'))
+limite = int(input('Digite o limite:'))
+maior_soma(n1, n2, limite)
